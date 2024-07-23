2d64f9ea0546ff0361673c38bef9bfc9f5b32c87
patriciadania <patricia.adania@hotmail.com>
2024-07-23 15:48:28 -0300

    Hierarquia de Componentes

---
+++ src/components/Pessoa.vue
@@ -0,0 +1,21 @@
+<template>    
+<div>
+    <h2> Esta é a descrição da pessoa: {{ nome }} </h2>
+    <Info/>
+  
+</div>
+</template>
+<script>
+import Info from './Info.vue';
+
+export default{
+    name: "Pessoa",
+    components: {
+        Info
+    },
+    data() {
+        return {
+            nome: "Joao"
+        }
+    }}
+    </script>
