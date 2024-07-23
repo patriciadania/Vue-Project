48c03757d23c2d4cb68a6b38db9d75c198b5bd2f
patriciadania <patricia.adania@hotmail.com>
2024-07-23 15:28:58 -0300

    Descrição das alterações

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
+  <div>
   <PrimeiroComponenteVue/>
-
+  <p>O dado de app é: {{  testando }}</p>
+  <LifeCycle/>
+</div>
 </template>
 
 <script>
 import PrimeiroComponenteVue from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue';
 
 export default{
   name: 'App',
   components: {
-    PrimeiroComponenteVue
+    PrimeiroComponenteVue,
+    LifeCycle
+  },
+  data(){
+    return {
+      testando: "Teste"
+    }
   }
-
 }
 </script>
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <h1> Meu nome é: {{ nome }}</h1>
+  
+  
+</template>
+
+<script>
+export default{
+    name: "LyfeCycle",
+    data() {
+        return {
+            nome: "Ainda nao sei",
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.nome = "Matheus"  
+        },1000)   
+    },
+    mounted(){
+        setTimeout(() => {
+            this.nome = "Pedro"  
+        },2000)
+    }
+}
+</script>
