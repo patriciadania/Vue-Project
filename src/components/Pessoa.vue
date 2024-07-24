b3f1e9cc7d633b6ba230bd8c248fc14b79185ac3
patriciadania <patricia.adania@hotmail.com>
2024-07-24 13:21:51 -0300

    Argumentos

--- src/components/Pessoa.vue
+++ src/components/Pessoa.vue
@@ -2,16 +2,19 @@
 <div>
     <h2> Esta é a descrição da pessoa: {{ nome }} </h2>
     <Info/>
+    <Form/>
   
 </div>
 </template>
 <script>
 import Info from './Info.vue';
+import Form from './Form.vue';
 
 export default{
     name: "Pessoa",
     components: {
-        Info
+        Info,
+        Form
     },
     data() {
         return {
