5a1329c28de87111b197a05b81bcebd6a12ab9e5
patriciadania <patricia.adania@hotmail.com>
2024-07-25 13:37:05 -0300

    Eventos e Multiplos Eventos

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,28 +1,53 @@
 <template>
-    <h2> Meu formulário: </h2>
-      <form action=" ">
-        <div>
-        <InputText/>
+  <h2>Meu formulário:</h2>
+  <form action=" " @submit="enviarFormulario($event)">
+    <div>
+      <input type="text" v-model="name">
     </div>
-<div>
-        <InputText/>
+    <div>
+      <input type="text" v-model="email">
     </div>
-<div>
-        <Submit/>
+    <div>
+      <Submit />
     </div>
-
-        </form>
-
+  </form>
+  <p>Conteúdo</p>
+  <p>Conteúdo</p>
+  <p>Conteúdo</p>
+  <p>Conteúdo</p>
 </template>
 <script>
-import InputText from './form/InputText.vue';
-import Submit from './form/Submit.vue';
+import InputText from "./form/InputText.vue";
+import Submit from "./form/Submit.vue";
 
-export default{
-    name: "Form",
-    components: {
-        InputText,
-        Submit
-    } 
-}
+export default {
+  name: "Form",
+  components: {
+    InputText,
+    Submit,
+  },
+  data(){
+    return {
+      name: "",
+      email: ""
+    }
+  },
+  methods: {
+    enviarFormulario(e) {
+      e.preventDefault();
+
+      const name = this.name;
+      const email = this.email;
+
+      console.log("ENVIADO UHUU");
+      console.log("O nome é " + name);
+      console.log("O email é " + email);
+    },
+  },
+};
 </script>
+<style scoped>
+p {
+  margin: 150px;
+}
+</style>
